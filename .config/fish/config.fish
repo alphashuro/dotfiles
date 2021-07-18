2c4ca82096536b25be5e8d24bb4f11171c3776b8
alpha <alphashuro@gmail.com>
2021-07-18 18:59:32 +0200

    Initial commit

---
+++ .config/fish/config.fish
@@ -0,0 +1,21 @@
+if status is-interactive
+  # autojump init
+  [ -f /usr/local/share/autojump/autojump.fish ]; and source /usr/local/share/autojump/autojump.fish
+  #asdf init
+  [ -f /usr/local/opt/asdf/asdf.fish ]; and source /usr/local/opt/asdf/asdf.fish
+  #nix init
+  if test -e '/Users/alpha/.nix-profile/etc/profile.d/nix.sh'
+    fenv source '/Users/alpha/.nix-profile/etc/profile.d/nix.sh'
+  end
+  # thefuck init
+  thefuck --alias | source
+  # direnv init (with asdf)
+  asdf exec direnv hook fish | source
+  function direnv
+    asdf exec direnv $argv
+  end
+
+  set -x FZF_DEFAULT_COMMAND 'fd --type f --hidden --follow --exclude .git'
+  set -x GPG_TTY (tty)
+end
+
---
+++ .config/nvim/lua/plugins.lua
@@ -0,0 +1,94 @@
+-- Install packer
+local install_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
+
+if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+  vim.fn.execute('!git clone https://github.com/wbthomason/packer.nvim ' .. install_path)
+end
+
+vim.api.nvim_exec(
+  [[
+  augroup Packer
+    autocmd!
+    autocmd BufWritePost init.lua PackerCompile
+  augroup end
+]],
+  false
+)
+
+require('packer').startup(function()
+  -- packer can manage itself
+  use 'wbthomason/packer.nvim'
+
+  -- the successor to solarized
+  use 'morhetz/gruvbox'
+
+  -- "gc" to comment visual regions/lines
+  use 'tpope/vim-commentary' 
+
+  -- configure and enhance the built in lsp
+  use 'neovim/nvim-lspconfig'
+  use 'hrsh7th/nvim-compe'
+
+  -- Highlight, edit, and navigate code using a fast incremental parsing library
+  use 'nvim-treesitter/nvim-treesitter'
+  -- Additional textobjects for treesitter
+  use 'nvim-treesitter/nvim-treesitter-textobjects'
+
+  -- fuzzy file finder
+  use {
+    'nvim-telescope/telescope.nvim',
+    requires = {
+      'nvim-lua/popup.nvim',
+      'nvim-lua/plenary.nvim',
+      'kyazdani42/nvim-web-devicons' -- icons
+    }
+  }
+
+  -- on demand language support 
+  use 'sheerun/vim-polyglot'
+
+  -- git integration
+  use 'tpope/vim-fugitive'
+
+  -- NERD TREE
+  use 'scrooloose/nerdtree'
+
+  -- editorconfig file support
+  use 'editorconfig/editorconfig-vim'
+
+  -- show git signs for added, removed and changed lines
+  use {
+    'lewis6991/gitsigns.nvim',
+    requires = {
+      'nvim-lua/plenary.nvim'
+    },
+    config = function()
+      require('gitsigns').setup()
+    end
+  }
+
+  -- fancy statusline
+  use 'itchyny/lightline.vim'
+
+  -- session management
+  use 'rmagatti/auto-session'
+
+  -- git blame
+  use 'APZelos/blamer.nvim'
+
+  -- surround magic
+  use 'tpope/vim-surround'
+
+  -- use separators , . ; : + - = ~ _ * # / | \ & $ and arguments (a) as text objects
+  -- e.g. ci_ = change inner _
+  use 'wellle/targets.vim' 
+
+  -- connect to dbs from vim
+  use 'tpope/vim-dadbod'
+  use 'kristijanhusak/vim-dadbod-ui' 
+
+  -- move across each word in camel/snake case
+  use 'bkad/CamelCaseMotion'
+end)
+
+require('configs/lspconfig')
