3fdbd930c5ab1059a0c69022cea25c14f5198ade
Alpha Shuro <alphashuro@gmail.com>
2024-08-03 11:42:24 +0200

    config updates

--- run_once_after_install-1-fish-setup.sh
+++ run_once_after_install-1-fish-setup.sh
@@ -4,3 +4,11 @@ curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install 
   | NONINTERACTIVE=1 fish
 
 fish -c 'omf install'
+
+sudo ln -si $(which fish) /usr/local/bin/fish
+
+if ! grep -q fish "/etc/shells"; then
+  echo /usr/local/bin/fish | sudo tee -a /etc/shells
+fi
+
+chsh -s /usr/local/bin/fish
