acd6f126d3f84453bb9064ed9f65f39db6e5d11f
Alpha Shuro <alphashuro@gmail.com>
2025-02-27 00:42:04 +0100

    add nvim zellij layout

---
+++ private_dot_config/zellij/scripts/executable_open-in-nvim-tab.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# go to hx tab, creating it if it doesn't exist yet
+zellij --layout nvim action go-to-tab-name -c "nvim"
+
+zellij action write 27 # send ESC to make sure we're in mormal mode
+zellij action write-chars ":edit $1"
+zellij action write 13 # send enter key to execute the command
